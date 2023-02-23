b0b43c627cf1c5bf62a6602e4849a0d5d7d24176
Nuri Akman <nuriakman@gmail.com>
2023-02-23 11:28:38 +0300

    Navigation, sadece Home ve Help olarak şekilde düzenlendi

--- pdo.router/src/App.vue
+++ pdo.router/src/App.vue
@@ -2,26 +2,17 @@
   <header>
     <div>
       <nav>
-        <RouterLink to="/"><b>Users</b></RouterLink>
+        <RouterLink to="/"><b>Home</b></RouterLink> &nbsp;
         <RouterLink to="/help"><b>Help</b></RouterLink> &nbsp;
-        <RouterLink to="/update"><b>Update User Informations</b></RouterLink>
-        <RouterLink to="/delete"><b>Delete User</b></RouterLink>
       </nav>
 
-      <listUser />
-      <updateUser />
-      <registerUser />
-      <deleteUser />
+      <RouterView></RouterView>
     </div>
   </header>
 </template>
 
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import listUser from "./components/listUser.vue";
-import registerUser from "./components/registerUser.vue";
-import updateUser from "./components/updateUser.vue";
-import deleteUser from "./components/deleteUser.vue";
 </script>
 
 <style scoped></style>
