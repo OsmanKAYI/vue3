7267d0a8e121807174a8d7213ad347abd4ba3a88
Osman KAYI <osmankayi06@gmail.com>
2023-03-12 17:00:58 +0300

    updated

---
+++ deneme/src/components/list.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-autocomplete
+          clearable
+          label="Nereden"
+          :items="[
+            'California',
+            'Colorado',
+            'Florida',
+            'Georgia',
+            'Texas',
+            'Wyoming',
+          ]"
+          variant="underlined"
+        ></v-autocomplete>
+      </v-col>
+      <v-col
+        ><v-autocomplete
+          clearable
+          label="Nereye"
+          :items="[
+            'California',
+            'Colorado',
+            'Florida',
+            'Georgia',
+            'Texas',
+            'Wyoming',
+          ]"
+          variant="underlined"
+        ></v-autocomplete
+      ></v-col>
+      <v-col cols="12" sm="6" md="4">
+        <v-menu
+          v-model="menu"
+          :close-on-content-click="false"
+          :nudge-right="40"
+          transition="scale-transition"
+          offset-y
+          min-width="290px"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+              v-model="date"
+              label="Yolculuk Tarihi"
+              prepend-icon="event"
+              readonly
+              v-bind="attrs"
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker v-model="date" @input="menu = false"></v-date-picker>
+        </v-menu>
+      </v-col>
+      <v-col> <v-btn>Sefer Bul</v-btn> </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    date: new Date().toISOString().substr(0, 10),
+    menu: false,
+    modal: false,
+  }),
+};
+</script>
--- deneme/src/views/Home.vue
+++ deneme/src/views/Home.vue
@@ -1,11 +1,13 @@
 <template>
   <headerr />
   <travel />
+  <list />
   <footerr />
 </template>
 
 <script setup>
 import headerr from "@/components/header.vue";
 import travel from "@/components/travel.vue";
+import list from "@/components/list.vue";
 import footerr from "@/components/footer.vue";
 </script>
