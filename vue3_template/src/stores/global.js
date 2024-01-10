d860a636aee635d3c485b87193281ca3ed0a93d2
Osman KAYI <osmankayi06@gmail.com>
2024-01-10 18:00:30 +0300

    ref and reactive deleted

--- vue3_template/src/stores/global.js
+++ vue3_template/src/stores/global.js
@@ -1,10 +1,9 @@
-import { ref } from 'vue'
+// import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useGlobalStore = defineStore('counter', () => {
-  const userName = ref('Osman Kayı')
-  const count = ref(0)
+  const userName = 'Osman Kayı'
+  const count = 0
 
   return { userName, count }
 })
-
