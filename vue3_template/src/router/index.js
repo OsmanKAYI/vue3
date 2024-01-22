ea0389418d011f85551ef409f6c09d7a45f4c640
Osman KAYI <osmankayi06@gmail.com>
2024-01-22 12:51:20 +0300

    ../views changed to @/views

--- vue3_template/src/router/index.js
+++ vue3_template/src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
-import PiniaView from '../views/PiniaView.vue'
-import VuetifyView from '../views/VuetifyView.vue'
+import HomeView from '@/views/HomeView.vue'
+import AboutView from '@/views/AboutView.vue'
+import PiniaView from '@/views/PiniaView.vue'
+import VuetifyView from '@/views/VuetifyView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
