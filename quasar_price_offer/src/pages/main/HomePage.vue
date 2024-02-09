b336c0f55ae7de75863035545f342c8472d59e7a
Osman KAYI <osmankayi06@gmail.com>
2024-02-09 17:00:11 +0300

    home page added

---
+++ quasar_price_offer/src/pages/main/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page padding>
+    <div class="text-h2">MAIN</div>
+    <div class="text-h6">Home Page</div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  // name: 'PageName'
+})
+</script>
