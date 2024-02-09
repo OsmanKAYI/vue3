7212d40649511e7e94c16d45bc630b463ce2201b
Osman KAYI <osmankayi06@gmail.com>
2024-02-09 17:06:31 +0300

    carousel component added

---
+++ quasar_price_offer/src/components/CarouselMain.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="q-pa-md">
+    <q-carousel animated v-model="slide" navigation infinite :autoplay="autoplay" arrows transition-prev="slide-right"
+      transition-next="slide-left" @mouseenter="autoplay = duration * duration" @mouseleave="autoplay = duration">
+      <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg" />
+      <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" />
+      <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
+      <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg" />
+    </q-carousel>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const slide = ref(1); // start at slide 1
+const duration = 1000;
+const autoplay = ref(duration);
+
+</script>
