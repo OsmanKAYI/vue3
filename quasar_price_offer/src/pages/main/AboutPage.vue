c8378992fbdb5b30557bf49a1fd2ba04079ac556
Osman KAYI <osmankayi06@gmail.com>
2024-02-12 16:46:45 +0300

    about page updated

--- quasar_price_offer/src/pages/main/AboutPage.vue
+++ quasar_price_offer/src/pages/main/AboutPage.vue
@@ -2,47 +2,52 @@
 </script>
 
 <template>
-  <q-page padding>
-    <div class="text-h6">MAIN</div>
-    <div class="text-subtitle1">About Page</div>
+  <q-page padding class=" justify-evenly">
+    <q-card class="q-pa-xl q-ma-md" bordered>
 
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
-      suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.Lorem ipsum dolor sit amet
-      consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
-      suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.Lorem ipsum dolor sit amet
-      consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
-      suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.Lorem ipsum dolor sit amet
-      consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
-      suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.Lorem ipsum dolor sit amet
-      consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
-      suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.Lorem ipsum dolor sit amet
-      consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
-      suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.Lorem ipsum dolor sit amet
-      consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
-      suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.Lorem ipsum dolor sit amet
-      consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
-      suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.Lorem ipsum dolor sit amet
-      consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
-      suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.Lorem ipsum dolor sit amet
-      consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
-      suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
-      suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
-      suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
-      suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
-      suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.
-    </p>
+      <div class="text-h6">MAIN</div>
+      <div class="text-subtitle1">About Page</div>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
+        suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
+        suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
+        suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
+        suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
+        suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
+        suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
+        suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
+        suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
+        suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
+        suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
+        suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
+        suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
+        suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
+        suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.
+      </p>
+    </q-card>
   </q-page>
 </template>
+
+<style scoped lang="scss"></style>
