1858985fbfe3ea3f498015995d4ebdb7f1ba964b
Osman KAYI <osmankayi06@gmail.com>
2024-02-12 16:27:40 +0300

    login page updated

--- quasar_price_offer/src/pages/main/LoginPage.vue
+++ quasar_price_offer/src/pages/main/LoginPage.vue
@@ -1,37 +1,3 @@
-<template>
-  <q-page class="q-pa-lg">
-    <div class="q-gutter-md row justify-center">
-      <q-card class="q-pa-md col-md-6 col-sm-9 col-xs-12">
-        <q-form @submit="onSubmit">
-          <div class="q-ml-sm items-center">
-
-            <q-card-section>
-              <div class="text-h6">LOGIN</div>
-              <div class="text-subtitle1">Login Page</div>
-            </q-card-section>
-
-            <q-input filled v-model="username" label="Username *" hint="will be used in login" lazy-rules
-              :rules="[val => val && val.length > 0 || 'Please type something']">
-              <template v-slot:prepend><q-icon name="face" color="grey" size="32px" /></template>
-            </q-input>
-
-            <q-input filled type="password" v-model="password" label="Password *" hint="must be at least 8 characters"
-              lazy-rules :rules="[val => val && val.length > 0 || 'Password must be at least 8 characters']">
-              <template v-slot:prepend><q-icon name="vpn_key" color="grey" size="32px" /></template>
-            </q-input>
-          </div>
-
-          <q-toggle v-model="rememberMe" label="Remember me!" />
-
-          <div class="row justify-center q-mt-md">
-            <q-btn label="Login" type="submit" color="primary" />
-          </div>
-        </q-form>
-      </q-card>
-    </div>
-  </q-page>
-</template>
-
 <script setup lang="ts">
 import { ref } from 'vue'
 import { useQuasar } from 'quasar'
@@ -60,3 +26,44 @@ function onSubmit() {
   }
 }
 </script>
+
+<template>
+  <q-page class="q-pa-lg">
+    <div class="q-gutter-md row">
+      <q-card class="q-pa-md col-md-6 col-sm-9 col-xs-12">
+        <q-form @submit="onSubmit">
+          <div class="q-ml-md">
+
+            <q-card-section>
+              <div class="text-h6">LOGIN</div>
+              <div class="text-subtitle1">Login Page</div>
+            </q-card-section>
+
+            <q-input filled v-model="username" label="Username *" hint="will be used in login" lazy-rules
+              :rules="[val => val && val.length > 0 || 'Please type something']">
+              <template v-slot:prepend><q-icon name="face" color="grey" size="32px" /></template>
+            </q-input>
+
+            <q-input filled type="password" v-model="password" label="Password *" hint="must be at least 8 characters"
+              lazy-rules :rules="[val => val && val.length > 0 || 'Password must be at least 8 characters']">
+              <template v-slot:prepend><q-icon name="vpn_key" color="grey" size="32px" /></template>
+            </q-input>
+          </div>
+
+          <q-toggle v-model="rememberMe" label="Remember me!" />
+
+          <div class="row q-mt-md">
+            <q-btn label="Login" type="submit" color="primary" />
+          </div>
+        </q-form>
+      </q-card>
+    </div>
+  </q-page>
+</template>
+
+<style scoped lang="css">
+div {
+  justify-items: center;
+  justify-content: center;
+}
+</style>
