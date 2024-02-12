d4e41dc1d90e2d8696691205a704b74d455c3aec
Osman KAYI <osmankayi06@gmail.com>
2024-02-12 14:03:42 +0300

    about page added

--- quasar_price_offer/src/pages/main/AboutPage.vue
+++ quasar_price_offer/src/pages/main/AboutPage.vue
@@ -1,13 +1,30 @@
 <template>
   <q-page padding>
-    <div class="text-h2">MAIN</div>
-    <div class="text-h6">About Page</div>
+    <div class="text-h6">MAIN</div>
+    <div class="text-subtitle1">About Page</div>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
+      suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
+      suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
+      suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
+      suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam illo nisi quam aperiam, qui totam ipsum eius ipsa
+      suscipit quis aliquam, cum illum sequi molestiae rerum animi facilis delectus odit.
+    </p>
   </q-page>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-export default defineComponent({
-  // name: 'PageName'
-})
+<script setup lang="ts">
 </script>
