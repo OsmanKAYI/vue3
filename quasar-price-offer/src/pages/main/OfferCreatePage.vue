2cb5480fc17b352d228454bc7cc1435ac9d23c48
Osman KAYI <osmankayi06@gmail.com>
2024-03-01 10:44:27 +0300

    todo added for scrolling operations on items table

--- quasar-price-offer/src/pages/main/OfferCreatePage.vue
+++ quasar-price-offer/src/pages/main/OfferCreatePage.vue
@@ -341,6 +341,7 @@ const addItem = () => {
             </div>
 
             <div class="row q-pt-xs">
+              <!-- TODO: Add scrolling operation when padding comes back -->
               <div class="col col-md-12 text-h6 q-pa-xs q-mb-xs text-weight-bold text-center" :class="generalTheme">
                 Items</div>
               <q-table class="col-12" bordered :rows="itemRows" :columns="columns" row-key="name" binary-state-sort
